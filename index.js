08048b87d79f42ccb422c61291685e1d4d1538ed
MVel1710 <55520345+MVel1710@users.noreply.github.com>
2022-03-02 23:52:29 -0500

    Added some functionality and styling

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
 
 const typingDiv = document.getElementById("typing");
-
+const statsDiv = document.getElementById("stats");
 console.log(typingDiv);
 
-//pregenerated text fornow , API for random text will be added later
-const text = "There was a limit to how creative you could be with the outdated means available."
+//pregenerated text for now , API for random text will be added later
+const paragraphs = [
+    'There was a',
+    'Space is the boundless three-dimensional extent in which objects',
+];
 
-//dynamically create spans that will be used for text coloring later on
-const characters = text.split("").map((char) => {
-    const span = document.createElement('span');
-    span.innerText = char;
-    typingDiv.appendChild(span);
-    return span;
-});
+const startGame = () => {
 
-cosnt = cursorIndex = 0;
-const firsCharacter = characters[cursorIndex]; 
-firsCharacter.classList.add('cursor');
+    typingDiv.innerHTML = " ";
+    statsDiv.innerHTML = " ";
 
-//typed characters
-document.addEventListener("keydown", ({key}) => {
-    console.log(key);
+    const text = paragraphs[parseInt(Math.random() * paragraphs.length)]; // grab random text from paragraphs
 
-    if (key == firsCharacter.innerText) {
-        firsCharacter.classList.remove("cursor");
-        firsCharacter.classList.add("done");
-        firsCharacter = characters[++cursorindex]; //increment location of cursor
+    //dynamically create spans that will be used for text coloring later on
+    const characters = text.split("").map((char) => {
+        const span = document.createElement('span');
+        span.innerText = char;
+        typingDiv.appendChild(span);
+        return span;
+    });
 
-    }
-});
+    let cursorIndex = 0;
+    let cursorCharacter = characters[cursorIndex]; 
+    cursorCharacter.classList.add('cursor');
+
+    let startTime = null;
+
+    //typed characters
+    const keyListener = document.addEventListener("keydown", ({key}) => {
+        //console.log(key);
+        if (!startTime){
+            startTime = new Date(); //when key is pressed startime is set to new date 
+        }
+
+        if (key === cursorCharacter.innerText) {
+            cursorCharacter.classList.remove("cursor");
+            cursorCharacter.classList.add("done");
+            cursorCharacter = characters[++cursorIndex]; //increment location of cursor - increment first to start at 1 not 0
+        }
+        if (cursorIndex >= characters.length){ // stThop timer when last character is typed
+            const endTime = new Date();
+            const delta = endTime - startTime; // time has passed between start and end 
+            const seconds = delta / 1000; // time passed in seconds 
+            const mins = seconds / 60; 
+            const numberOfWords = text.split(" ").length; // how many words
+            const wps = numberOfWords / seconds;
+            const wpm = wps * 60.0;
+            document.getElementById("stats").innerText = `wpm = ${wpm}`;
+            //display cpm and wpm
+            document.removeEventListener("keydown", keyListener);
+
+            return;
+        }
+        cursorCharacter.classList.add("cursor"); 
+    });
+};
 
