e289b18305b849b6b3940a8a59680d5a1ac09808
MVel1710 <55520345+MVel1710@users.noreply.github.com>
2022-03-05 23:31:06 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const typingDiv = document.getElementById("typing");
 const statsDiv = document.getElementById("stats");
+const startGameBtn = document.getElementById("start-game");
 console.log(typingDiv);
 
 //pregenerated text for now , API for random text will be added later
@@ -11,8 +12,9 @@ const paragraphs = [
 
 const startGame = () => {
 
-    typingDiv.innerHTML = " ";
-    statsDiv.innerHTML = " ";
+    startGameBtn.classList.add("hidden");
+    typingDiv.innerHTML = "";
+    statsDiv.innerHTML = "";
 
     const text = paragraphs[parseInt(Math.random() * paragraphs.length)]; // grab random text from paragraphs
 
@@ -30,33 +32,37 @@ const startGame = () => {
 
     let startTime = null;
 
+    const keydown = ({key}) => {
+            //console.log(key);
+            if (!startTime){
+                startTime = new Date(); //when key is pressed startime is set to new date 
+            }
+    
+            if (key === cursorCharacter.innerText) {
+                cursorCharacter.classList.remove("cursor");
+                cursorCharacter.classList.add("done");
+                cursorCharacter = characters[++cursorIndex]; //increment location of cursor - increment first to start at 1 not 0
+            }
+            //game ends
+            if (cursorIndex >= characters.length){ // stThop timer when last character is typed
+                const endTime = new Date();
+                const delta = endTime - startTime; // time has passed between start and end 
+                const seconds = delta / 1000; // time passed in seconds 
+                const mins = seconds / 60; 
+                const numberOfWords = text.split(" ").length; // how many words
+                const wps = numberOfWords / seconds;
+                const wpm = wps * 60.0;
+                document.getElementById("stats").innerText = `wpm = ${wpm}`;
+                //display cpm and wpm
+                document.removeEventListener("keydown", keydown);
+                startGameBtn.classList.remove("hidden");
+                return;
+            }
+            cursorCharacter.classList.add("cursor"); 
+        };
+    
     //typed characters
-    const keyListener = document.addEventListener("keydown", ({key}) => {
-        //console.log(key);
-        if (!startTime){
-            startTime = new Date(); //when key is pressed startime is set to new date 
-        }
-
-        if (key === cursorCharacter.innerText) {
-            cursorCharacter.classList.remove("cursor");
-            cursorCharacter.classList.add("done");
-            cursorCharacter = characters[++cursorIndex]; //increment location of cursor - increment first to start at 1 not 0
-        }
-        if (cursorIndex >= characters.length){ // stThop timer when last character is typed
-            const endTime = new Date();
-            const delta = endTime - startTime; // time has passed between start and end 
-            const seconds = delta / 1000; // time passed in seconds 
-            const mins = seconds / 60; 
-            const numberOfWords = text.split(" ").length; // how many words
-            const wps = numberOfWords / seconds;
-            const wpm = wps * 60.0;
-            document.getElementById("stats").innerText = `wpm = ${wpm}`;
-            //display cpm and wpm
-            document.removeEventListener("keydown", keyListener);
-
-            return;
-        }
-        cursorCharacter.classList.add("cursor"); 
-    });
+    document.addEventListener("keydown", keydown);
+    
 };
 
